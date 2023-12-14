5b6dad09775b410c89078e4e378a56820f1a7eec
Frederico Malaquias <fmacaldeira@sga.pucminas.br>
2023-12-13 21:29:16 -0300

    OPF

--- Grafo.java
+++ Grafo.java
@@ -3,34 +3,34 @@ import java.io.IOException;
 import java.util.*;
 
 public class Grafo {
-    private final int numeroDeVertices;
-    private final List<List<Aresta>> listaDeAdjacencia;
+    private final int numVertices;
+    private final List<List<Aresta>> listaAdj;
 
-    public Grafo(int numeroDeVertices) {
-        this.numeroDeVertices = numeroDeVertices;
-        listaDeAdjacencia = new ArrayList<>();
+    public Grafo(int numVertices) {
+        this.numVertices = numVertices;
+        listaAdj = new ArrayList<>();
 
-        for (int i = 0; i < numeroDeVertices; ++i) {
-            listaDeAdjacencia.add(new ArrayList<>());
+        for (int i = 0; i < numVertices; ++i) {
+            listaAdj.add(new ArrayList<>());
         }
     }
 
     public void adicionarAresta(int origem, int destino, int peso) {
         Aresta aresta = new Aresta(destino, peso);
-        listaDeAdjacencia.get(origem).add(aresta);
+        listaAdj.get(origem).add(aresta);
     }
 
-    public void criarGrafoConexoAleatorio(int numeroDeArestas) {
+    public void criarGrafoConexoAleatorio(int numArestas) {
         Random random = new Random();
 
-        for (int i = 0; i < numeroDeArestas; i++) {
-            int origem = random.nextInt(numeroDeVertices);
-            int destino = random.nextInt(numeroDeVertices);
+        for (int i = 0; i < numArestas; i++) {
+            int origem = random.nextInt(numVertices);
+            int destino = random.nextInt(numVertices);
             int peso = random.nextInt(50) + 1;
 
             while (origem == destino || arestaExiste(origem, destino) || arestaExiste(destino, origem)) {
-                origem = random.nextInt(numeroDeVertices);
-                destino = random.nextInt(numeroDeVertices);
+                origem = random.nextInt(numVertices);
+                destino = random.nextInt(numVertices);
             }
 
             adicionarAresta(origem, destino, peso);
@@ -38,7 +38,7 @@ public class Grafo {
     }
 
     private boolean arestaExiste(int origem, int destino) {
-        for (Aresta aresta : listaDeAdjacencia.get(origem)) {
+        for (Aresta aresta : listaAdj.get(origem)) {
             if (aresta.getDestino() == destino) {
                 return true;
             }
@@ -48,20 +48,20 @@ public class Grafo {
 
     public void salvarGrafoCSV(String nomeArquivo) {
         try (FileWriter writer = new FileWriter(nomeArquivo)) {
-            for (int i = 0; i < numeroDeVertices; ++i) {
-                for (Aresta aresta : listaDeAdjacencia.get(i)) {
+            for (int i = 0; i < numVertices; ++i) {
+                for (Aresta aresta : listaAdj.get(i)) {
                     writer.append(i + "," + aresta.getDestino() + "," + aresta.getPeso() + "\n");
                 }
             }
             writer.flush();
         } catch (IOException e) {
-            System.out.println("Ocorreu um erro ao salvar o arquivo CSV: " + e.getMessage());
+            System.out.println("Erro ao salvar o arquivo CSV: " + e.getMessage());
         }
     }
 
     public int[] calcularLimites(int[] x) {
-        int limiteInferior = numeroDeVertices - 1;
-        int limiteSuperior = numeroDeVertices * (numeroDeVertices - 1) / 2;
+        int limiteInferior = numVertices - 1;
+        int limiteSuperior = numVertices * (numVertices - 1) / 2;
 
         for (int i = 0; i < x.length; i++) {
             x[i] = limiteInferior + (i * (limiteSuperior - limiteInferior) / 4);
@@ -70,17 +70,14 @@ public class Grafo {
         return x;
     }
 
-    // Algoritmo Bellman Ford
-    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     public void bellmanFord(int verticeOrigem) {
-        int[] distancias = new int[numeroDeVertices];
+        int[] distancias = new int[numVertices];
         Arrays.fill(distancias, Integer.MAX_VALUE);
         distancias[verticeOrigem] = 0;
 
-        // Relaxamento das arestas repetido |V| - 1 vezes
-        for (int i = 1; i < numeroDeVertices; ++i) {
-            for (int u = 0; u < numeroDeVertices; ++u) {
-                for (Aresta aresta : listaDeAdjacencia.get(u)) {
+        for (int i = 1; i < numVertices; ++i) {
+            for (int u = 0; u < numVertices; ++u) {
+                for (Aresta aresta : listaAdj.get(u)) {
                     int v = aresta.getDestino();
                     int peso = aresta.getPeso();
 
@@ -91,9 +88,8 @@ public class Grafo {
             }
         }
 
-        // Verifica se há ciclo negativo
-        for (int u = 0; u < numeroDeVertices; ++u) {
-            for (Aresta aresta : listaDeAdjacencia.get(u)) {
+        for (int u = 0; u < numVertices; ++u) {
+            for (Aresta aresta : listaAdj.get(u)) {
                 int v = aresta.getDestino();
                 int peso = aresta.getPeso();
 
@@ -104,19 +100,17 @@ public class Grafo {
             }
         }
 
-        // Imprime as distâncias mínimas após o Bellman-Ford
-        System.out.println("Distancias minimas apo1s o Bellman-Ford:");
-        for (int i = 0; i < numeroDeVertices; ++i) {
+        System.out.println("Distâncias mínimas após o Bellman-Ford:");
+        for (int i = 0; i < numVertices; ++i) {
             System.out.println("Vertice " + i + ": " + distancias[i]);
         }
     }
 
- public int[][] floydWarshall() {
-        int[][] dist = new int[numeroDeVertices][numeroDeVertices];
+    public int[][] floydWarshall() {
+        int[][] dist = new int[numVertices][numVertices];
 
-        // Inicializa a matriz de distâncias
-        for (int i = 0; i < numeroDeVertices; i++) {
-            for (int j = 0; j < numeroDeVertices; j++) {
+        for (int i = 0; i < numVertices; i++) {
+            for (int j = 0; j < numVertices; j++) {
                 if (i == j) {
                     dist[i][j] = 0;
                 } else {
@@ -125,17 +119,15 @@ public class Grafo {
             }
         }
 
-        // Preenche a matriz com as distâncias conhecidas do grafo
-        for (int i = 0; i < numeroDeVertices; i++) {
-            for (Aresta aresta : listaDeAdjacencia.get(i)) {
+        for (int i = 0; i < numVertices; i++) {
+            for (Aresta aresta : listaAdj.get(i)) {
                 dist[i][aresta.getDestino()] = aresta.getPeso();
             }
         }
 
-        // Algoritmo Floyd-Warshall
-        for (int k = 0; k < numeroDeVertices; k++) {
-            for (int i = 0; i < numeroDeVertices; i++) {
-                for (int j = 0; j < numeroDeVertices; j++) {
+        for (int k = 0; k < numVertices; k++) {
+            for (int i = 0; i < numVertices; i++) {
+                for (int j = 0; j < numVertices; j++) {
                     if (dist[i][k] != Integer.MAX_VALUE / 2 &&
                             dist[k][j] != Integer.MAX_VALUE / 2 &&
                             dist[i][k] + dist[k][j] < dist[i][j]) {
@@ -145,16 +137,14 @@ public class Grafo {
             }
         }
 
-        // Imprime as distâncias mínimas após o Floyd-Warshall
         System.out.println("Distâncias mínimas após o Floyd-Warshall:");
-        for (int i = 0; i < numeroDeVertices; ++i) {
-            for (int j = 0; j < numeroDeVertices; ++j) {
+        for (int i = 0; i < numVertices; ++i) {
+            for (int j = 0; j < numVertices; ++j) {
                 System.out.print(dist[i][j] + " ");
             }
             System.out.println();
         }
 
-        // Salva as distâncias do algoritmo de Floyd-Warshall em um arquivo
         salvarDistanciasFloydWarshall(dist);
 
         return dist;
@@ -163,8 +153,8 @@ public class Grafo {
     private void salvarDistanciasFloydWarshall(int[][] distancias) {
         String nomeArquivo = "distancias_floyd_warshall.txt";
         try (FileWriter writer = new FileWriter(nomeArquivo)) {
-            for (int i = 0; i < numeroDeVertices; ++i) {
-                for (int j = 0; j < numeroDeVertices; ++j) {
+            for (int i = 0; i < numVertices; ++i) {
+                for (int j = 0; j < numVertices; ++j) {
                     writer.append(distancias[i][j] + " ");
                 }
                 writer.append("\n");
@@ -172,25 +162,16 @@ public class Grafo {
             writer.flush();
             System.out.println("Distâncias salvas em " + nomeArquivo);
         } catch (IOException e) {
-            System.out.println("Ocorreu um erro ao salvar o arquivo de distâncias: " + e.getMessage());
+            System.out.println("Erro ao salvar o arquivo de distâncias: " + e.getMessage());
         }
     }
 
-
-
-
-
-
-
-
-    // Algoritmo Dijkstra
-    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     public Map<Integer, Integer> dijkstra(int verticeOrigem) {
         Map<Integer, Integer> distancias = new HashMap<>();
         PriorityQueue<VerticeDistancia> filaPrioridade = new PriorityQueue<>(
                 Comparator.comparingInt(VerticeDistancia::getDistancia));
 
-        for (int i = 0; i < numeroDeVertices; ++i) {
+        for (int i = 0; i < numVertices; ++i) {
             distancias.put(i, Integer.MAX_VALUE);
         }
 
@@ -200,7 +181,7 @@ public class Grafo {
         while (!filaPrioridade.isEmpty()) {
             int verticeAtual = filaPrioridade.poll().getVertice();
 
-            for (Aresta aresta : listaDeAdjacencia.get(verticeAtual)) {
+            for (Aresta aresta : listaAdj.get(verticeAtual)) {
                 int destino = aresta.getDestino();
                 int novaDistancia = distancias.get(verticeAtual) + aresta.getPeso();
 
@@ -214,21 +195,57 @@ public class Grafo {
         return distancias;
     }
 
-    private static class VerticeDistancia {
-        private final int vertice;
-        private final int distancia;
+    public void executarOPF(int verticeOrigem) {
+        long inicio = System.nanoTime();
 
-        public VerticeDistancia(int vertice, int distancia) {
-            this.vertice = vertice;
-            this.distancia = distancia;
+        int numVertices = this.numVertices;
+        boolean[] visitado = new boolean[numVertices];
+        int[] distancia = new int[numVertices];
+        Arrays.fill(distancia, Integer.MAX_VALUE);
+
+        distancia[verticeOrigem] = 0;
+
+        PriorityQueue<Integer> filaPrioridade = new PriorityQueue<>(numVertices,
+                Comparator.comparingInt(o -> distancia[o]));
+        filaPrioridade.offer(verticeOrigem);
+
+        while (!filaPrioridade.isEmpty()) {
+            int u = filaPrioridade.poll();
+
+            if (visitado[u])
+                continue;
+
+            visitado[u] = true;
+
+            for (Aresta aresta : listaAdj.get(u)) {
+                int v = aresta.getDestino();
+                int pesoArestaUV = aresta.getPeso();
+
+                int newDistance = Math.max(distancia[u], pesoArestaUV);
+
+                if (newDistance < distancia[v]) {
+                    distancia[v] = newDistance;
+                    filaPrioridade.offer(v);
+                }
+            }
         }
 
-        public int getVertice() {
-            return vertice;
-        }
+        long fim = System.nanoTime();
+        salvarTempo(numVertices, "OPF", inicio, fim);
+    }
 
-        public int getDistancia() {
-            return distancia;
+    private static void salvarTempo(int numVertices, String algoritmo, long inicio, long fim) {
+        String nomeArquivo = "tempos_execucao.txt";
+
+        try (FileWriter writer = new FileWriter(nomeArquivo, true)) {
+            long tempoTotal = fim - inicio;
+
+            writer.write("Algoritmo: " + algoritmo + "\n");
+            writer.write("Número de Vértices: " + numVertices + "\n");
+            writer.write("Tempo de execução: " + tempoTotal + " nanossegundos\n");
+            writer.write("-------------------------------------\n");
+        } catch (IOException e) {
+            System.out.println("Erro ao salvar o tempo de execução: " + e.getMessage());
         }
     }
 
@@ -247,6 +264,24 @@ public class Grafo {
 
         public int getPeso() {
             return peso;
+        }
+    }
+
+    private static class VerticeDistancia {
+        private final int vertice;
+        private final int distancia;
+
+        public VerticeDistancia(int vertice, int distancia) {
+            this.vertice = vertice;
+            this.distancia = distancia;
+        }
+
+        public int getVertice() {
+            return vertice;
+        }
+
+        public int getDistancia() {
+            return distancia;
         }
     }
 }
--- Main.java
+++ Main.java
@@ -4,53 +4,56 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numeroDeArestas = new int[5];
-        long inicio, fim, tempoTotal;
+        int[] numArestasArray = new int[5];
+        long startTime, endTime, totalTime;
 
-        for (int x = 1; x <= 5; x++) {
-            numeroDeArestas = new Grafo((int) Math.pow(5, x)).calcularLimites(numeroDeArestas);
+        for (int power = 1; power <= 5; power++) {
+            numArestasArray = new Grafo((int) Math.pow(5, power)).calcularLimites(numArestasArray);
 
             for (int i = 0; i < 5; i++) {
-                int numeroDeVertices = (int) Math.pow(5, x);
+                int numVertices = (int) Math.pow(5, power);
 
-                System.out.println("Número de vértices: " + numeroDeVertices);
-                System.out.println("Número de arestas: " + numeroDeArestas[i]);
+                System.out.println("Número de vértices: " + numVertices);
+                System.out.println("Número de arestas: " + numArestasArray[i]);
 
-                String nomeArquivo = "meu_grafo_v" + x + "_grafo" + i + ".csv";
-                Grafo meuGrafo = new Grafo(numeroDeVertices);
+                String fileName = "meu_grafo_v" + power + "_grafo" + i + ".csv";
+                Grafo myGraph = new Grafo(numVertices);
 
-                inicio = System.currentTimeMillis(); // Grava o tempo de início
+                startTime = System.currentTimeMillis(); // Grava o tempo de início
 
-                meuGrafo.criarGrafoConexoAleatorio(numeroDeArestas[i]);
-                meuGrafo.salvarGrafoCSV(nomeArquivo);
+                myGraph.criarGrafoConexoAleatorio(numArestasArray[i]);
+                myGraph.salvarGrafoCSV(fileName);
 
                 // Executa o algoritmo de Dijkstra
-                int verticeOrigemDijkstra = 0; // Escolha o vértice de origem desejado
-                Map<Integer, Integer> distanciasDijkstra = meuGrafo.dijkstra(verticeOrigemDijkstra);
+                int sourceVertexDijkstra = 0; // Escolha o vértice de origem desejado
+                Map<Integer, Integer> distancesDijkstra = myGraph.dijkstra(sourceVertexDijkstra);
 
                 // Executa o algoritmo de Bellman-Ford
-                int verticeOrigemBellman = 0; // Escolha o vértice de origem desejado
-                meuGrafo.bellmanFord(verticeOrigemBellman);
+                int sourceVertexBellman = 0; // Escolha o vértice de origem desejado
+                myGraph.bellmanFord(sourceVertexBellman);
 
                 // Executa o algoritmo de Floyd-Warshall
-                meuGrafo.floydWarshall();
+                myGraph.floydWarshall();
+                
+                int sourceVertexOPF = 0; // Escolha o vértice de origem desejado
+                myGraph.executarOPF(sourceVertexOPF);
 
                 System.out.println("--------------------");
-                meuGrafo = new Grafo(numeroDeVertices);
+                myGraph = new Grafo(numVertices);
 
-                fim = System.currentTimeMillis(); // Grava o tempo de fim
-                tempoTotal = fim - inicio;
+                endTime = System.currentTimeMillis(); // Grava o tempo de fim
+                totalTime = endTime - startTime;
 
-                gravarTempoDeExecucao(tempoTotal, "tempo_execucao.txt");
+                gravarTempoDeExecucao(totalTime, "tempo_execucao.txt");
             }
         }
     }
 
-    private static void gravarTempoDeExecucao(long tempo, String nomeArquivo) {
-        try (FileWriter writer = new FileWriter(nomeArquivo, true)) {
+    private static void gravarTempoDeExecucao(long tempo, String fileName) {
+        try (FileWriter writer = new FileWriter(fileName, true)) {
             writer.write("Tempo de execução total: " + tempo + " milissegundos\n");
         } catch (IOException e) {
-            System.out.println("Ocorreu um erro ao gravar o tempo de execução: " + e.getMessage());
+            System.out.println("Erro ao gravar o tempo de execução: " + e.getMessage());
         }
     }
 }
